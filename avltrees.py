3b9cf2f1f6ff0adad8f8e413eb8e0a3bc0bfb6a8
ssykkur <ashembered@gmail.com>
2022-07-28 16:35:34 +0300

    modified avltrees

--- avltrees.py
+++ avltrees.py
@@ -1,225 +1,236 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
-
-
 class Node:
+
     def __init__(self, data, parent=None):
         self.data = data
+        self.left_node = None
+        self.right_node = None
         self.parent = parent
-        self.left = None
-        self.right = None
         self.height = 0
 
 
-class AvlTree:
+class AVLTree:
+
     def __init__(self):
         self.root = None
-    
+
     def insert(self, data):
         if self.root is None:
             self.root = Node(data)
-            print(f'root node is {self.root.data}')
         else:
-            self._insert(data, self.root)
-    
-    def _insert(self, data, node):
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
         if data < node.data:
-            if node.left:
-                self._insert(data, node.left)
+            if node.left_node:
+                self.insert_node(data, node.left_node)
             else:
-                node.left = Node(data, node)
-                node.height = max(self._calc_height(node.left), self._calc_height(node.right)) + 1
-                logging.debug(f'inserted left node {node.left.data} and parent height now is {node.height}')
+                node.left_node = Node(data, node)
+                node.height = max(self.calc_height(node.left_node), self.calc_height(node.right_node)) + 1
         else:
-            if node.right:
-                self._insert(data, node.right)
+            if node.right_node:
+                self.insert_node(data, node.right_node)
             else:
-                node.right = Node(data, node)
-                node.height = max(self._calc_height(node.left), self._calc_height(node.right)) + 1
-                logging.debug(f'inserted right node {node.right.data} and parent height now is {node.height}')
-        self._balance_check(node)
+                node.right_node = Node(data, node)
+                node.height = max(self.calc_height(node.left_node), self.calc_height(node.right_node)) + 1 
+        
+        self.handle_violation(node)
 
     def remove(self, data):
         if self.root:
-            self._remove(data, self.root)
+            self.remove_node(data, self.root)
 
-    def _remove(self, data, node):
+    def remove_node(self, data, node):
+
         if node is None:
             return
-
         if data < node.data:
-            self._remove(data, node.left)
+            self.remove_node(data, node.left_node)
         elif data > node.data:
-            self._remove(data, node.right)
-        else:
-            parent = node.parent
-        
-            # leaf
-            if node.left is None and node.right is None:
-                logging.debug(f'removing leaf node: {node.data}')
+            self.remove_node(data, node.right_node)
 
+        else:
+            if node.left_node is None and node.right_node is None:
+                parent = node.parent
+                if parent is not None:
+                    if parent.left_node == node:
+                        parent.left_node = None
+                    else:
+                        parent.right_node = None
                 if parent is None:
                     self.root = None
+                del node 
+                self.handle_violation(parent)
+
+            elif node.left_node is None and node.right_node is not None:
+                parent = node.parent
+                if parent is not None:
+                    if parent.left_node == node:
+                        parent.left_node = node.right_node
+                    if parent.right_node == node:
+                        parent.right_node = node.right_node
                 else:
-                    if parent.left == node:
-                        parent.left = None
-                    else:
-                        parent.right = None
-                    self._balance_check(parent)
+                    self.root = node.right_node
+                node.right_node.parent = parent
                 del node
+                self.handle_violation(parent)
 
-            # one left child
-            elif node.left is not None and node.right is None:
-                logging.debug(f'removing one left child: {node.data}')
-                
-                if parent is None:
-                    node.left.parent = None
-                    self.root = node.left
+            elif node.left_node is not None and node.right_node is None:
+                parent = node.parent
+                if parent is not None:
+                    if parent.left_node == node:
+                        parent.left_node = node.left_node
+                    if parent.right_node == node:
+                        parent.right_node = node.left_node
                 else:
-                    if parent.left == node:
-                        parent.left = node.left
-                    else:
-                        parent.right = node.left
-                    node.left.parent = parent
-                    self._balance_check(parent)
+                    self.root = node.left_node
+                node.right_node.parent = parent
                 del node
+                self.handle_violation(parent)
 
-            # one right child
-            elif node.left is None and node.right is not None:
-                logging.debug(f'removing one right child: {node.data}')
-
-                if parent is None:
-                    node.right.parent = None
-                    self.root = node.right 
-                else:
-                    if parent.left == node:
-                        parent.left = node.right
-                    else:
-                        parent.right = node.right
-                    node.right.parent = parent
-                    self._balance_check(parent)
-                del node
-
-            # two children
             else:
-                logging.debug(f'removing two children: {node.data}')
-                predecessor = self._get_predecessor(node.left)
+                predecessor = self.get_predecessor(node.left_node)
 
                 temp = predecessor.data
                 predecessor.data = node.data
                 node.data = temp
-                self._remove(data, predecessor)
 
-    def _get_predecessor(self, node):
-        while node.right:
-            node = node.right
+                self.remove_node(data, predecessor)
+            
+    def get_predecessor(self, node):
+        if node.right_node:
+            self.get_predecessor(node.right_node)
         return node
-
-    def _balance_check(self, node):
-        while node is not None:
-            node.height = max(self._calc_height(node.left), 
-                                self._calc_height(node.right)) + 1
-            self._enforce_balance(node)
-            node = node.parent
-    
-    def _enforce_balance(self, node):
-        balance = self._calc_balance(node)
-        logging.debug(f'balance of node {node.data} is {balance}')
-        if balance > 1:
-            if self._calc_balance(node.left) < 0:
-                self._rotate_left(node.left)
-            self._rotate_right(node)
-        elif balance < -1:
-            if self._calc_balance(node.right) > 0:
-                self._rotate_right(node.right)
-            self._rotate_left(node)
-    
-    def _rotate_left(self, node):
-        logging.debug(f'rotating to left on {node.data} with height: {node.height}')
-        temp_right = node.right
-        t = temp_right.left
-        temp_right.left = node 
-        node.right = t
-
-    
-        if t:
-            t.parent = node
-
-        temp_right.parent = node.parent
-        node.parent = temp_right
-
-        if temp_right.parent and temp_right.parent.left == node:
-            temp_right.parent.left = temp_right
-        if temp_right.parent and temp_right.parent.right == node:
-            temp_right.parent.right = temp_right
-        
-        if node == self.root:
-            self.root = temp_right
-        
-        node.height = max(self._calc_height(node.left), 
-                        self._calc_height(node.right)) + 1
-
-        temp_right.height = node.height + 1
-        
-
-    def _rotate_right(self, node):
-        logging.debug(f'rotating to right on {node.data} with height: {node.height}')
-        
-        temp_left = node.left
-        t = temp_left.right
-        temp_left.right = node
-        node.left = t
-
-        if t:
-            t.parent = node
-        
-        temp_left.parent = node.parent
-        node.parent = temp_left
-
-        parent_left = temp_left.parent
-        
-        if parent_left and parent_left.left == node:
-            parent_left.left = temp_left
-        if parent_left and parent_left.right == node:
-            parent_left.right = temp_left 
-        
-        if node == self.root:
-            self.root = temp_left
-        
-        node.height = max(self._calc_height(node.left), 
-                        self._calc_height(node.right)) + 1
-
-        temp_left.height = node.height + 1
-
-    def _calc_height(self, node):
-        if node is None:
-            return - 1
-        return node.height
-
-    def _calc_balance(self, node):
-        if node is None:
-            return 0
-        return self._calc_height(node.left) - self._calc_height(node.right)
 
     def traverse(self):
         if self.root:
-            self._traverse(self.root)
+            self.traverse_in_order(self.root)
+
+    def traverse_in_order(self, node):
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+        
+        l = ''
+        r = ''
+        p = ''
+
+        if node.left_node is not None:
+            l = node.left_node.data
+        else:
+            l = 'NULL'
+        
+        if node.left_node is not None:
+            r = node.right_node.data
+        else:
+            r = 'NULL'
+
+        if node.parent is not None:
+            p = node.parent
+        else:
+            p = 'NULL'
+        
+        print(f'{l} left node; {r} right node; {p} parent. node\'s height {node.height}')    
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+    def rotate_right(self, node):
+        print(f"right rotation on {node.data}")
+        temp_left_node = node.left_node
+        t = temp_left_node.right_node
+        temp_left_node.right_node = node
+        node.left_node = t
+
+        if t is not None:
+            t.parent = node
+
+        temp_parent = node.parent
+        node.parent = temp_left_node
+        temp_left_node.parent = temp_parent
+
+        if temp_left_node.parent is not None and temp_left_node.parent.left_node == node:
+            temp_left_node.parent.left_node = temp_left_node
+        if temp_left_node.parent is not None and temp_left_node.parent.right_node == node:
+            temp_left_node.parent.right_node = temp_left_node
+
+        if node == self.root:
+            self.root = temp_left_node
+
+        node.height = max(self.calc_height(node.left_node),
+                            self.calc_height(node.right_node)) + 1
+
+        temp_left_node.height = max(self.calc_height(temp_left_node.left_node),
+                                    self.calc_height(temp_left_node.right_node)) + 1
+
+    def rotate_left(self, node):
+        print(f'left rotation on {node.data}')
+        temp_right_node = node.right_node
+        t = temp_right_node.left_node
+        temp_right_node.left_node = node
+        node.right_node = t
+
+        if t is not None:
+            t.parent = node
+        
+        temp_parent = node.parent
+        node.parent = temp_right_node
+        temp_right_node.parent = temp_parent
+        
+        if temp_right_node.parent is not None and temp_right_node.parent.left_node == node:
+            temp_right_node.parent.left_node = temp_right_node
+        if temp_right_node.parent is not None and temp_right_node.parent.left_node == node:
+            temp_right_node.parent.right_node = temp_right_node
+        
+        if node == self.root:
+            self.root = temp_right_node
+
+        node.height = max(self.calc_height(node.left_node), self.calc_height(node.right_node)) + 1
+
+        temp_right_node.height = max(self.calc_height(temp_right_node.left_node), 
+                                    self.calc_height(temp_right_node.right_node)) + 1
+
+
+    def handle_violation(self, node):
+        while node is not None:
+            node.height = max(self.calc_height(node.left_node), 
+                                self.calc_height(node.right_node)) + 1
+            self.violation_helper(node)
+            # going up the tree checking for inbalance and making rotations accordingly 
+            node = node.parent
+
+    def violation_helper(self, node):
+        balance = self.calculate_balance(node)
+
+        if balance > 1:
+            if self.calculate_balance(node.left_node) < 0:
+                self.rotate_left(node.left_node)
+            self.rotate_right(node)
+        if balance < -1:
+            if self.calculate_balance(node.right_node) > 0:
+                self.rotate_right(node.right_node)
+            self.rotate_left(node)
+
+    def calc_height(self, node):
+        # when node is a NULL pointer
+        if node is None:
+            return -1
+        return node.height
     
-    def _traverse(self, node):
-        if node.left:
-            self._traverse(node.left)
-        print(node.data)
-        if node.right:
-            self._traverse(node.right)
+    def calculate_balance(self, node):
+        if node is None:
+            return 0
+        return self.calc_height(node.left_node) - self.calc_height(node.right_node)
+
+
+tree = AVLTree()
+tree.insert(5)
+tree.insert(3)
+tree.insert(10)
+tree.insert(2)
+tree.insert(4)
+tree.insert(15)
+tree.remove(15)
+tree.remove(10)
+
+
 
     
-        
-if __name__ == '__main__':
-    avl = AvlTree()
-    avl.insert(20)
-    avl.insert(15)
-    avl.insert(16)
-    avl.insert(30)
-    avl.remove(30)
-    avl.traverse()
